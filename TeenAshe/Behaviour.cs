ffe4738ebec5a3aa6dfdc97701e8238eda1b2f95
I AM THAT I AM <kauan.rmk@gmail.com>
2014-11-19 15:38:34 -0200

    Teen Ashe
    My first attempt at a L# Assemnly

---
+++ TeenAshe/Behaviour.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeenageAshe
+{
+    struct Behaviour
+    {
+        public bool UseQ { get; set; }
+        public bool UseW { get; set; }
+        public bool UseE { get; set; }
+        public bool UseR { get; set; }
+    }
+}
